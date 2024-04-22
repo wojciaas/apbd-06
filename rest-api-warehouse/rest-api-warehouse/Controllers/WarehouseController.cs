3f42ad8776abdc03fc46d1e4018e9f04b8235d17
s27994 <s27994@pjwstk.edu.pl>
2024-04-22 15:54:21 +0200

    changed routes

---
+++ rest-api-warehouse/rest-api-warehouse/Controllers/WarehouseController.cs
@@ -0,0 +1,6 @@
+namespace rest_api_warehouse.Controllers;
+
+public class WarehouseController
+{
+    
+}
--- sql-rest-api/sql-rest-api/Animal/Configuration.cs
+++ sql-rest-api/sql-rest-api/Animal/Configuration.cs
@@ -4,17 +4,17 @@ public static class Configuration
 {
     public static IEndpointRouteBuilder RegisterAnimalEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/animals", (HttpRequest request, IAnimalService service) =>
+        endpoints.MapGet("api/animals", (HttpRequest request, IAnimalService service) =>
         {
             string orderBy = request.Query["orderBy"].FirstOrDefault() ?? "name";
             return TypedResults.Ok(service.GetAnimals(orderBy));
         });
-        endpoints.MapPost("/animals", (AnimalDTO animalDTO, IAnimalService service) => TypedResults.Created("", service.CreateAnimal(animalDTO)));
-        endpoints.MapPut("/animals/{id:int}", (int id, AnimalDTO animalDTO, IAnimalService service) =>
+        endpoints.MapPost("api/animals", (AnimalDTO animalDTO, IAnimalService service) => TypedResults.Created("", service.CreateAnimal(animalDTO)));
+        endpoints.MapPut("api/animals/{id:int}", (int id, AnimalDTO animalDTO, IAnimalService service) =>
         {
             return service.UpdateAnimal(animalDTO, id) == 0 ? (IResult) TypedResults.NotFound() : TypedResults.NoContent();
         });
-        endpoints.MapDelete("/animals/{id:int}", (int id, IAnimalService service) =>
+        endpoints.MapDelete("api/animals/{id:int}", (int id, IAnimalService service) =>
         {
             return service.DeleteAnimal(id) == 0 ? (IResult) TypedResults.NotFound() : TypedResults.NoContent();
         });
